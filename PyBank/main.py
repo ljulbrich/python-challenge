7ddfb7af57143cfd56a7604bde0acd85cfab32e2
ljulbrich <lucasulbrich@protonmail.com>
2023-12-29 20:52:06 +1100

    Completed PyBank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,29 +1,28 @@
 import csv
 from pathlib import Path
-import numpy
+import statistics
 
 # Creating variables
+txt_file = open('Analysis/data_analysis.txt', 'w+')
 csv_path = Path('Resources/budget_data.csv')
 month_count = 0
 net_total = 0
 average_change = 0
-greatest_increase = 0
-greatest_increase_month = None
-greatest_decrease = 0
-greatest_decrease_month = None
-
-
+increase = []
+decrease = []
 
 if __name__ == "__main__":
 
     # This opens up the CSV file which uses ',' as a delimiter
-    with open(csv_path, newline='') as csvfile:
+    with (open(csv_path, newline='') as csvfile):
         budget_data = csv.reader(csvfile, delimiter=',', quotechar='|')
 
         # Create internal variables
         header = [next(budget_data)]
         row_data = []
         profit_loss = []
+        change = []
+        register = 0
 
         # row_data will store all the data without the headers
         for row in budget_data:
@@ -32,18 +31,44 @@ if __name__ == "__main__":
         # This returns the amount of months in the csv file
         month_count = len(row_data)
 
-        # This adds the profit/loss column together
+        # This adds the profit/loss column together and creates a
+        # list of only the profit/loss column
         for row in row_data:
             net_total += int(row[1])
-            profit_loss.append(row[1])
+            profit_loss.append(int(row[1]))
 
-        print(profit_loss)
-        average_change = numpy.mean(profit_loss)
+        # This column calculates the change between each month and
+        # then calculates the average of that change
+        for row in profit_loss:
+            if register == 0:
+                register = row
+            else:
+                change.append(row - register)
+                register = row
 
-        print(f"Financial Analysis\n"
-              f"-------------------------------------\n"
-              f"Total Months: {month_count}\n"
-              f"Total: {net_total}\n"
-              f"Average Change: {average_change}\n"
-              f"Greatest Increase in Profits: {greatest_increase_month} ({greatest_increase})\n"
-              f"Greatest Decrease in Profits: {greatest_decrease_month} ({greatest_decrease})\n")
+        average_change = statistics.mean(change)
+
+        # These two variables store the largest increases and decreases as a list
+        increase.append(max(profit_loss))
+        decrease.append(min(profit_loss))
+
+        # This loop will go through the data to add the month to the second item in the lists
+        for i in row_data:
+            if int(i[1]) == increase[0]:
+                increase.append(i[0])
+            elif int(i[1]) == decrease[0]:
+                decrease.append(i[0])
+
+        # This is the output string
+        output = (f'Financial Analysis\n'
+                  f'-------------------------------------\n'
+                  f'Total Months: {month_count}\n'
+                  f'Total: ${net_total}\n'
+                  f'Average Change: ${average_change}\n'
+                  f'Greatest Increase in Profits: {increase[1]} (${increase[0]})\n'
+                  f'Greatest Decrease in Profits: {decrease[1]} (${decrease[0]})\n')
+
+        # This section prints the output to the terminal and then saves it as a text file
+        print(output)
+        txt_file.write(output)
+        txt_file.close()
